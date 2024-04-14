0fd85f688f18fc94ec538c1a90eef4c6c4639b48
Daniil Kurganov <dk30128@gmail.com>
2024-04-14 17:46:47 +0300

    Реализация окна авторизации

--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
 import sys
+import psycopg2
+from PyQt5.QtWidgets import QMessageBox
 from WindowAuthorization import *
 
 def authorization() -> None:
     '''Процесс авторизации'''
+    global sting_password, string_privilege
     with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM services')
-        print('{}'.format(cursor.fetchall()))
-def show_error_message(int_error_key, string_error_massage: str) -> None:
+        string_request = "SELECT * FROM accounts WHERE login = %s AND passwd = %s"
+        cursor.execute(string_request, (str(ui.TextEditLoginInput.toPlainText()), str(ui.TextEditPasswordInput.toPlainText())))
+        tuple_authorization_result = cursor.fetchone()
+        if tuple_authorization_result != None: sting_password, string_privilege = tuple_authorization_result[2], tuple_authorization_result[3]
+        else: show_error_message(13, 'Пользователь с такими данными не существует.')
+        return None
+def show_error_message(int_error_key: int, string_error_massage: str) -> None:
     '''Вывод ошибок'''
-    dictionary_error_texts = {6: 'Ошибка подключения к БД'}
+    dictionary_error_texts = {6: 'Ошибка подключения к БД', 13: 'Ошибка авторизации'}
     message_error = QMessageBox()
     message_error.setIcon(QMessageBox.Critical)
     message_error.setText(dictionary_error_texts[int_error_key])
@@ -22,8 +29,9 @@ WindowAuthorization = QtWidgets.QMainWindow()
 ui = Ui_WindowAuthorization()
 ui.setupUi(WindowAuthorization)
 WindowAuthorization.show()
-try: connection = psycopg2.connect(host = 'localhost', user = 'postgres', password = '123456789', dbname = 'SoftwareCompanyDB', port = 5432)
-except Exception as string_error: show_error_message(string_error)
-finally:
-    if connection: connection.close()
+try:
+    connection = psycopg2.connect(host = 'localhost', user = 'postgres', password = '123456789', dbname = 'SoftwareCompanyDB', port = 5432)
+    ui.PushButtonAuthorization.clicked.connect(authorization)
+except Exception as string_error: show_error_message(6, string_error)
 sys.exit(app.exec_())
+if connection: connection.close()
